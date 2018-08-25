c2a4dd84d3c7e235cd501d2515f2066e23f5affd
Matthew Huberty <matthew.huberty@gmail.com>
2018-08-25 13:17:41 -0700

    More changes

---
+++ Push_Pull.js
@@ -0,0 +1,13 @@
+//Items to push to the server:
+//1.  Name of customer
+//2.  Phone Number
+//3.  Email address of customer
+//4.  
+//The API call 
+
+//Items to pull from the server:
+//1.  Current reservations database table. 
+//2.  Current waiting list database table.
+//The API call would ask for current reservations OR
+//The API call would ask for a waiting list 
+//The last API call would allow the user to clear reservations.
